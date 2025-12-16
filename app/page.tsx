03c3e452648fafd63c68ab25d43407e1e300899d
Reuel Rai <reuelrai2001@gmail.com>
2025-12-17 01:25:19 +0545

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
     <div className="flex flex-col justify-center items-center h-screen text-green-700">
       <h1>This is the first page</h1>
-      <h2>This page is under construction.</h2>
+      <h2></h2>
     </div>
     </>
   );
