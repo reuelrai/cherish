40499624165b8feca6a123c2e8fb217bad77aae8
Reuel Rai <reuelrai2001@gmail.com>
2025-12-17 22:31:55 +0545

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div className="flex flex-col justify-center items-center h-screen text-green-700">
-      <h1>This is the first page</h1>
+    <div className="flex flex-col justify-center items-center h-screen ">
+      <h1 className="text-black hover:text-green-800">This is the first page</h1>
       <h2></h2>
     </div>
     </>
