123cc1bf0bacd180bf8753878cca326e15bf9488
Reuel Rai <reuelrai2001@gmail.com>
2025-12-18 23:12:39 +0545

    minor changes

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
         <header className="flex justify-center items-center h-screen flex-col text-center">
             <h2 className="text-4xl font-semibold">Welcome</h2>
             <p className="mt-4 text-muted-foreground">
-                Building ideas into reality.
+                Practice
             </p>
         </header>
     );
